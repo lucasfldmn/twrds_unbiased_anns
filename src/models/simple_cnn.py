9b36deafb4255b91269acf99e26e4ee7560edbfa
Lucas Feldmann <lucfeldmann23@googlemail.com>
2021-03-03 15:31:24 +0100

    added functional model api and created config with zero red shapes

--- src/models/simple_cnn.py
+++ src/models/simple_cnn.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Layer, Conv2D, MaxPool2D, Flatten, Dense
+from tensorflow.keras.layers import Input, Layer, Conv2D, MaxPool2D, Flatten, Dense
 
 
 class SimpleCNN(tf.keras.Model):
@@ -27,3 +27,18 @@ class SimpleCNN(tf.keras.Model):
         for layer in self.network_layers:
           x = layer(x)
         return x
+
+# Create model via functional API
+def get_simple_cnn():
+  model_input = Input(shape=(360, 360, 3), name="input_img")
+  x = Conv2D(32, (3, 3), padding = "same", activation = "relu", name = "conv_1")(model_input)
+  x = MaxPool2D(pool_size = (2, 2), name = "pool_1")(x)
+  x = Conv2D(64, (3, 3), padding = "same", activation = "relu", name = "conv_2")(x)
+  x = MaxPool2D(pool_size = (2, 2), name = "pool_2")(x)
+  x = Conv2D(64, (3, 3), padding = "same", activation = "relu", name = "conv_3")(x)
+  x = Flatten(name = "flat_1")(x)
+  x = Dense(100, activation='relu', name = "dense_1")(x)
+  x = Dense(20, activation='relu', name = "dense_2")(x)
+  model_output = Dense(1, name = "dense_3")(x)
+  return tf.keras.Model(model_input, model_output, name = "Simple_CNN")
+
